bbebdaa97a6f15aa1a9d6209b8f9a6b1e08d1713
Brian Parker <brian.parker@hotmail.com.au>
2021-12-27 12:09:27 +1100

    wip

---
+++ BDialogCoreDemo/Pages/Index.razor.cs
@@ -0,0 +1,52 @@
+using BJSInteroptCore.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace BDialogCoreDemo.Pages
+{
+    public partial class Index : ComponentBase
+    {
+
+        private bool isResizing = false;
+        private int resizeCount = 0;
+        private int resizingCount = 0;
+
+        [Inject]
+        private IJSInteropCoreService? JSInteropCoreService { get; set; }
+        protected override void OnInitialized()
+        {
+            JSInteropCoreService!.OnResize += Index_OnResize;
+            JSInteropCoreService!.OnResizing += Index_OnResizing;
+            base.OnInitialized();
+        }
+
+        private void Index_OnResizing(bool isResizing)
+        {
+            this.isResizing = isResizing;
+            resizingCount++;
+            InvokeAsync(StateHasChanged);
+        }
+
+        private void Index_OnResize()
+        {
+            resizeCount++;
+            InvokeAsync(StateHasChanged);
+        }
+
+        private ElementReference aButton;
+        private ElementReference anInput;
+
+        private void ButtonClicked()
+        {
+
+        }
+
+        private void Onfocusout()
+        {
+
+        }
+        private void Onfocusin()
+        {
+
+        }
+    }
+}
--- BDialogCoreDemo/Program.cs
+++ BDialogCoreDemo/Program.cs
@@ -1,11 +1,14 @@
+using BDialogCore.Extensions;
 using BDialogCoreDemo;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.RootComponents.Add<App>(selector: "#app");
+builder.RootComponents.Add<HeadOutlet>(selector: "head::after");
+
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBDialogCore();
 
 await builder.Build().RunAsync();
